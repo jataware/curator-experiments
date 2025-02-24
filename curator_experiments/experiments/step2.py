d8327f952e93377238b437dc3447716be203b8ef
David Samson <david.andrew.engineer@gmail.com>
2025-02-24 14:57:05 -0500

    some cleanup to separate the test case from the loop over multiple cases

--- curator_experiments/experiments/step2.py
+++ curator_experiments/experiments/step2.py
@@ -19,57 +19,70 @@ import pdb
 here = Path(__file__).parent
 
 
-def test(num_trials=10, timeout_seconds=60):
-    
+
+def main():
+    with move_to_isolated_dir():
+        test_loop(num_trials=10, timeout_seconds=600)
+
+
+
+class CaptureCode:
+    def __init__(self):
+        self.i = 0
+        self.code = defaultdict(list)
+    def set_i(self, i):
+        self.i = i
+    def __call__(self, code):
+        self.code[self.i].append(code)
+
+
+def test_case(query:str, capture_code:CaptureCode):
     # Set up AdhocApi with GDC API
     api = load_yaml_api(here/'../gdc/api_no_examples.yaml')
     adhoc_api = AdhocApi(
         apis=[api],
         drafter_config={'provider': 'google', 'model': 'gemini-1.5-pro-001'}
-        # drafter_config={'provider': 'openai', 'model': 'gpt-4o'}
     )
 
-    # set up the python tool to save any instances of code being run
-    class CaptureCode:
-        def __init__(self):
-            self.i = 0
-            self.code = defaultdict(list)
-        def set_i(self, i):
-            self.i = i
-        def __call__(self, code):
-            self.code[self.i].append(code)
-    capture_code=CaptureCode()
     python = PythonTool(code_side_effect=capture_code)
 
     # Set up archytas agent
     tools = [adhoc_api, python]
-
-
-    # query to test repeatability of
-    query_template = 'In GDC find all cases of lymphoblastic leukemia with a JAK1 somatic mutation and save the result to a csv named {name}'
-
-    # work in an isolated directory
-    with move_to_isolated_dir():
-        for i in range(num_trials):
-            print('='*80)
-            print(f"Trial {i}")
-            capture_code.set_i(i)
-            agent = ReActAgent(messages=[], model='gpt-4o', tools=tools, verbose=True, allow_ask_user=False)
-            query = query_template.format(name=f"trial_{i}.csv")
-            try:
-                with timeout(timeout_seconds):
-                    answer = agent.react(query)
-                    print(answer)
-            except (FailedTaskError, TimeoutException) as e:
-                print(f"Error: {e}")
+    agent = ReActAgent(messages=[], model='gpt-4o', tools=tools, verbose=True, allow_ask_user=False)
+    # query = query_template.format(name=f"trial_{i}.csv")
+    try:
+        answer = agent.react(query)
+        print(answer)
+    except (FailedTaskError, TimeoutException) as e:
+        print(f"Error: {e}")
             
-            print('='*80)
 
+
+def test_loop(num_trials: int, timeout_seconds: int):
+    # query to test repeatability of
+    query_template = 'In GDC find all cases of lymphoblastic leukemia with a JAK1 somatic mutation and save the result to a csv named {name}. Please do not print out the result, only save it to the csv file'
+
+    capture_code = CaptureCode()
+    for i in range(num_trials):
+        print('='*80)
+        print(f"Trial {i}")
+        capture_code.set_i(i)
+        with timeout(timeout_seconds):
+            test_case(
+                query_template.format(name=f'trial_{i}.csv'),
+                capture_code=capture_code
+            )
+        print('='*80)
 
     # Save all of the captured code to a file
     with open('captured_code.yaml', 'w') as f:
         yaml.dump(capture_code.code, f)
 
+
+
+def save_to_yaml(data, filename):
+    #TODO: better formatting of saved code with block lines rather than escaping whitespace
+    pdb.set_trace()
 
 
 
@@ -153,4 +166,4 @@ def characterize_deviation():
 
 
 if __name__ == "__main__":
-    test()
+    main()
