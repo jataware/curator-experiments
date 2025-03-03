db4cd1dc4103d417417f7650096b1c2db83f6204
David Samson <david.andrew.engineer@gmail.com>
2025-03-03 15:20:30 -0500

    adjusted analysis to cache llm rankings, default to o3-mini, and adjusted plot colors

--- curator_experiments/experiments/analysis.py
+++ curator_experiments/experiments/analysis.py
@@ -1,4 +1,5 @@
 import yaml
+import json
 from pathlib import Path
 import pandas as pd
 from dataclasses import dataclass
@@ -15,10 +16,13 @@ from .utils import timeout
 import pdb
 
 here = Path(__file__).parent
-workdir = here / '../../workdir_20250225_132405'
+workdir, task_variant = here / '../../workdir_20250225_132405', '(GDC w/ No Examples)'                 # step 2: no examples
+# workdir, task_variant = here / '../../workdir_20250228_134952', '(GDC w/ Verbatim Solution Example)'   # step 3a: single verbatim example
+# workdir, task_variant = here / '../../workdir_20250303_134057', '(GDC w/ Single Similar Example)'      # step 3b: single similar example
+
 
 def main():
-    analyze_step2()
+    analyze_trials()
 
 
 @dataclass
@@ -29,15 +33,15 @@ class Score:
     created_file: bool
     # code_match: float #TODO: future will have an agent compare the code
 
-def analyze_step2():
+def analyze_trials():
     # load the reference solution
     reference_path = here / 'ssm_occurrences_lymphoblastic_leukemia_JAK1.csv'
     reference = pd.read_csv(reference_path)
     solution_ids = set(reference['id'].tolist())
+    solution_num_ids = len(solution_ids)
 
 
     # load all the trials
-    # workdir = here / '../../workdir_20250225_132405'
     code_path = workdir/'captured_code.yaml'
     trials: dict = yaml.safe_load(code_path.open())
 
@@ -45,11 +49,12 @@ def analyze_step2():
     reference_code_path = here / 'step1.py'
     reference_code = reference_code_path.read_text()
     reference_code = reference_code.split('"""')[-1].strip()  # remove the docstring
-    # code_spread = measure_code_spread(reference_code, trials)
-    code_clusters = measure_code_clusters(reference_code, trials)
 
     # save the score of each trial
     scores: dict[str, Score] = {}
+
+    # save a list of the trials that are correct
+    solution_trials: list[str] = []
 
     # compare each of the trials to the reference
     for trial, code_chunks in tqdm(trials.items(), desc='Analyzing trials', total=len(trials)):
@@ -80,16 +85,15 @@ def analyze_step2():
         # see if the ids are correct
         correct_ids = len(solution_ids.intersection(data_ids)) / len(solution_ids)
 
-        #TODO: analyze the code chunks for how well they match
-        # code_chunks...
-
-
         # save the score
         scores[trial] = Score(correct_ids, correct_num_rows, any_data, True)
 
+        # save the trial if it is correct
+        if correct_ids == 1: solution_trials.append(trial)
+
     
     # print out a map from each result to the frequency of the result
-    correct_id_counts = [int(score.correct_ids * 8) for score in scores.values()]
+    correct_id_counts = [int(score.correct_ids * solution_num_ids) for score in scores.values()]
     correct_id_scores = set(correct_id_counts)
     correct_id_freqs = {score: correct_id_counts.count(score) for score in correct_id_scores}
     print(f"Correct ID Frequencies: {correct_id_freqs}")
@@ -99,7 +103,7 @@ def analyze_step2():
     # plt.xticks(range(0, 9))
     # plt.xlabel(f'Number of Correct IDs (max {len(solution_ids)})')
     # plt.ylabel('Number of Trials')
-    # plt.title('Frequency of Correct Results (No Examples Case)')
+    # plt.title(f'Frequency of Correct Results {task_variant}')
     # plt.savefig(workdir/'correct_ids_histogram.png')
     # plt.show()
 
@@ -108,7 +112,7 @@ def analyze_step2():
     # plt.hist(num_rows, bins=range(0, 3), align='left', rwidth=0.8)
     # plt.xlabel('Number of Rows in Result')
     # plt.ylabel('Number of Trials')
-    # plt.title('Frequency of Correct Number of Rows (No Examples Case)')
+    # plt.title(f'Frequency of Correct Number of Rows {task_variant}')
     # plt.savefig(workdir/'num_rows_histogram.png')
     # plt.show()
 
@@ -117,7 +121,7 @@ def analyze_step2():
     # plt.hist(any_data, bins=range(0, 3), align='left', rwidth=0.8)
     # plt.xlabel('Any Data in Result')
     # plt.ylabel('Number of Trials')
-    # plt.title('Frequency of Any Data in Result (No Examples Case)')
+    # plt.title(f'Frequency of Any Data in Result {task_variant}')
     # plt.savefig(workdir/'any_data_histogram.png')
     # plt.show()
 
@@ -126,9 +130,19 @@ def analyze_step2():
     # plt.hist(created_file, bins=range(0, 3), align='left', rwidth=0.8)
     # plt.xlabel('File Created')
     # plt.ylabel('Number of Trials')
-    # plt.title('Frequency of File Created (No Examples Case)')
+    # plt.title(f'Frequency of File Created {task_variant}')
     # plt.savefig(workdir/'created_file_histogram.png')
     # plt.show()
+
+
+    # plot a histogram of correct vs incorrect
+    correct = ['correct' if correct_id_count == solution_num_ids else 'incorrect' for correct_id_count in correct_id_counts]
+    plt.hist(correct, bins=range(0, 3), align='left', rwidth=0.8)
+    plt.xlabel('Success')
+    plt.ylabel('Number of Trials')
+    plt.title(f'Frequency of Correct Results {task_variant}')
+    plt.savefig(workdir/'correct_histogram.png')
+    plt.show()
 
 
     # make a single unified plot
@@ -136,41 +150,40 @@ def analyze_step2():
     any_data = [score.any_data for score in scores.values()]
     created_file = [score.created_file for score in scores.values()]
     xs = np.arange(len(scores))
-    plt.plot(xs, [int(i==8) for i in correct_id_counts], label='Correct IDs', marker='x')
+    plt.plot(xs, [i==solution_num_ids for i in correct_id_counts], label='Correct IDs', marker='x')
     # plt.scatter(xs, correct_num_rows, label='Correct Number of Rows', marker='+')
     # plt.scatter(xs, any_data, label='Any Data', marker='1')
     # plt.scatter(xs, created_file, label='File Created', marker='2')
     plt.xlabel('Trial')
     plt.ylabel('Score')
-    plt.title('Scores for Each Trial (No Examples Case)')
+    plt.title(f' Trial Successes {task_variant}')
     plt.legend()
     plt.savefig(workdir/'combined_scores_plot.png')
     plt.show()
 
 
-    # TODO: plotting of the clustering of the code solutions itself. 
+    # plotting of the clustering of the code solutions itself. 
+    plot_code_clusters(reference_code, trials, solution_trials) # mostly just the charts generated are what is of interest
 
 
-def measure_code_clusters(reference: str, trials: dict[str, list[str]], N:int=10):
-    model_name = 'gpt-4o' # Be sure to adjust the model in the trial function
-    
-    # Include the reference in the trials
-    reference_trial_number = len(trials)
-    while f'trial_{reference_trial_number}' in trials:
-        reference_trial_number += 1
-    reference_name = f'trial_{reference_trial_number}'
-    trials = {**trials, reference_name: [reference]}
-    # primary_ranking = primary_ranking + [(-1, reference_name, 100)]
+def have_llm_rank_code_spread(reference: str, trials: dict[str, list[str]], N:int=10):
+    # check if the cache file exists
+    cache_path = workdir/'code_clusters_cache.json'
+    if cache_path.exists():
+        with cache_path.open('r') as f:
+            all_rankings = json.load(f)
+        print(f"Loaded LLM rankings for {workdir.stem} from cache")
+        return all_rankings
 
     # get the primary list of rankings
+    print(f'No cache found. Generating code spread rankings for {workdir.stem} ...')
     primary_ranking = measure_code_spread_trial(reference, trials)
-
 
     # select N other evenly spaced trials from the primary ranking
     selections = np.linspace(0, len(primary_ranking)-1, N).astype(int)[1:]
     secondary_references_names = [primary_ranking[i][1] for i in selections]
     secondary_references = ['\n\n############\n\n'.join(trials[name]) for name in secondary_references_names]
-    all_secondary_rankings = []
+    all_secondary_rankings: list[list[tuple[int, str, float]]] = []
     for secondary_reference in tqdm(secondary_references, desc='measuring secondary references', total=len(secondary_references)):
         try:
             with timeout(300):
@@ -180,12 +193,30 @@ def measure_code_clusters(reference: str, trials: dict[str, list[str]], N:int=10
             continue
         all_secondary_rankings.append(secondary_ranking)
         sleep(5) # to avoid hitting the API too hard
+    
+    # save all rankings to a cache file
+    all_rankings = [primary_ranking] + all_secondary_rankings
+    cache_path = workdir/'code_clusters_cache.json'
+    with cache_path.open('w') as f:
+        json.dump(all_rankings, f, indent=4)
+    
+    return all_rankings
+
+def plot_code_clusters(reference: str, trials: dict[str, list[str]], solution_trials:list[str], N:int=10):
+    # Include the reference in the trials
+    reference_trial_number = len(trials)
+    while f'trial_{reference_trial_number}' in trials:
+        reference_trial_number += 1
+    reference_name = f'trial_{reference_trial_number}'
+    trials = {**trials, reference_name: [reference]}
+    
+    # collect the rankings from the llm (or load from cache)
+    all_rankings = have_llm_rank_code_spread(reference, trials, N)
 
     # dictionaries for building the feature vectors
     rank_feature_dict: dict[str, list[int]] = defaultdict(list)
     score_feature_dict: dict[str, list[float]] = defaultdict(list)
 
-    all_rankings = [primary_ranking] + all_secondary_rankings
     for rankings in all_rankings:
         # generate a dictionary out of the rankings for easy lookup
         ranking_map: dict[str, tuple[int, float]] = {}
@@ -212,63 +243,63 @@ def measure_code_clusters(reference: str, trials: dict[str, list[str]], N:int=10
     rank_features = np.array([rank_feature_dict[trial] for trial in trials])
     score_features = np.array([score_feature_dict[trial] for trial in trials])
 
-    solution_trials = ['trial_12', 'trial_24', 'trial_34', 'trial_58']
+    # separate the correct solutions (and reference) from the rest
     solution_indices = [list(trials.keys()).index(trial) for trial in solution_trials]
     reference_index = [list(trials.keys()).index(reference_name)]
+    exclude_indices = set(solution_indices + reference_index)
+    remaining_indices = [i for i in range(len(trials)) if i not in exclude_indices]
 
-    # plot the features using t-sne (2 separate plots). Highlight the solution trials with red x's
-    from sklearn.manifold import TSNE
-    tsne = TSNE(n_components=2, perplexity=30, max_iter=1000)
-    rank_tsne = tsne.fit_transform(rank_features)
-    score_tsne = tsne.fit_transform(score_features)
-    plt.scatter(rank_tsne[:, 0], rank_tsne[:, 1])
-    plt.scatter(rank_tsne[solution_indices, 0], rank_tsne[solution_indices, 1], color='red', marker='x', s=100)
-    plt.scatter(rank_tsne[reference_index, 0], rank_tsne[reference_index, 1], edgecolors='green', facecolors='none', marker='o', s=100)
-    plt.legend(['all trials', 'correct solutions', 'reference solution'],)
-    plt.title('t-SNE of Code Similarity Ranks')
-    plt.xlabel('t-SNE 1')
-    plt.ylabel('t-SNE 2')
-    plt.savefig(workdir/f'rank_tsne_({model_name}).png')
-    plt.show()
+    # # plot the features using t-sne (2 separate plots). Highlight the solution trials with red x's
+    # from sklearn.manifold import TSNE
+    # tsne = TSNE(n_components=2, perplexity=30, max_iter=1000)
+    # rank_tsne = tsne.fit_transform(rank_features)
+    # score_tsne = tsne.fit_transform(score_features)
+    # plt.scatter(rank_tsne[:, 0], rank_tsne[:, 1])
+    # plt.scatter(rank_tsne[solution_indices, 0], rank_tsne[solution_indices, 1], color='red', marker='x', s=100)
+    # plt.scatter(rank_tsne[reference_index, 0], rank_tsne[reference_index, 1], edgecolors='green', facecolors='none', marker='o', s=100)
+    # plt.legend(['all trials', 'correct solutions', 'reference solution'],)
+    # plt.title('t-SNE of Code Similarity Ranks')
+    # plt.xlabel('t-SNE 1')
+    # plt.ylabel('t-SNE 2')
+    # plt.savefig(workdir/f'rank_tsne_({model_name}).png')
+    # plt.show()
     
-    plt.scatter(score_tsne[:, 0], score_tsne[:, 1])
-    plt.scatter(score_tsne[solution_indices, 0], score_tsne[solution_indices, 1], color='red', marker='x', s=100)
-    plt.scatter(score_tsne[reference_index, 0], score_tsne[reference_index, 1], edgecolors='green', facecolors='none', marker='o', s=100)
-    plt.legend(['all trials', 'correct solutions', 'reference solution'],)
-    plt.title('t-SNE of Code Similarity Scores')
-    plt.xlabel('t-SNE 1')
-    plt.ylabel('t-SNE 2')
-    plt.savefig(workdir/f'score_tsne_({model_name}).png')
-    plt.show()
+    # plt.scatter(score_tsne[:, 0], score_tsne[:, 1])
+    # plt.scatter(score_tsne[solution_indices, 0], score_tsne[solution_indices, 1], color='red', marker='x', s=100)
+    # plt.scatter(score_tsne[reference_index, 0], score_tsne[reference_index, 1], edgecolors='green', facecolors='none', marker='o', s=100)
+    # plt.legend(['all trials', 'correct solutions', 'reference solution'],)
+    # plt.title('t-SNE of Code Similarity Scores')
+    # plt.xlabel('t-SNE 1')
+    # plt.ylabel('t-SNE 2')
+    # plt.savefig(workdir/f'score_tsne_({model_name}).png')
+    # plt.show()
 
     # All of the above plotting but with PCA instead
     from sklearn.decomposition import PCA
     pca = PCA(n_components=2)
-    rank_pca = pca.fit_transform(rank_features)
+    # rank_pca = pca.fit_transform(rank_features)
     score_pca = pca.fit_transform(score_features)
-    plt.scatter(rank_pca[:, 0], rank_pca[:, 1])
-    plt.scatter(rank_pca[solution_indices, 0], rank_pca[solution_indices, 1], color='red', marker='x', s=100)
-    plt.scatter(rank_pca[reference_index, 0], rank_pca[reference_index, 1], edgecolors='green', facecolors='none', marker='o', s=100)
-    plt.legend(['all trials', 'correct solutions', 'reference solution'],)
-    plt.title('PCA of Code Similarity Ranks')
-    plt.xlabel('PCA 1')
-    plt.ylabel('PCA 2')
-    plt.savefig(workdir/f'rank_pca_({model_name}).png')
-    plt.show()
+    # plt.scatter(rank_pca[:, 0], rank_pca[:, 1])
+    # plt.scatter(rank_pca[solution_indices, 0], rank_pca[solution_indices, 1], color='red', marker='x', s=100)
+    # plt.scatter(rank_pca[reference_index, 0], rank_pca[reference_index, 1], edgecolors='green', facecolors='none', marker='o', s=100)
+    # plt.legend(['all trials', 'correct solutions', 'reference solution'],)
+    # plt.title('PCA of Code Similarity Ranks')
+    # plt.xlabel('PCA 1')
+    # plt.ylabel('PCA 2')
+    # plt.savefig(workdir/f'rank_pca_({model_name}).png')
+    # plt.show()
 
-    plt.scatter(score_pca[:, 0], score_pca[:, 1])
-    plt.scatter(score_pca[solution_indices, 0], score_pca[solution_indices, 1], color='red', marker='x', s=100)
-    plt.scatter(score_pca[reference_index, 0], score_pca[reference_index, 1], edgecolors='green', facecolors='none', marker='o', s=100)
-    plt.legend(['all trials', 'correct solutions', 'reference solution'],)
-    plt.title('PCA of Code Similarity Scores')
+    plt.scatter(score_pca[remaining_indices, 0], score_pca[remaining_indices, 1], color='red', marker='.', s=25)
+    plt.scatter(score_pca[reference_index, 0], score_pca[reference_index, 1], edgecolors='green', facecolors='green', marker='*', s=100)
+    plt.scatter(score_pca[solution_indices, 0], score_pca[solution_indices, 1], edgecolors='white', facecolors='blue', marker='o', s=50)
+    plt.legend(['failed trials', 'success trials', 'reference solution'],)
+    plt.title('Code Similarity')
     plt.xlabel('PCA 1')
     plt.ylabel('PCA 2')
-    plt.savefig(workdir/f'score_pca_({model_name}).png')
+    plt.savefig(workdir/f'score_pca.png')
     plt.show()
 
     
-    pdb.set_trace()
-    ...
 
 def measure_code_spread(reference: str, trials: dict[str, list[str]], n_repeats: int = 10) -> list[tuple[int, str, float]]:
     all_rankings = [measure_code_spread_trial(reference, trials) for _ in tqdm(range(n_repeats), desc='Collecting Code Spread Trials', total=n_repeats)]
@@ -316,7 +347,7 @@ def measure_code_spread_trial(reference: str, trials: dict[str, list[str]]) -> l
     # print(trials_str)
 
     # agent = ClaudeAgent(model='claude-3-5-sonnet-latest', system_prompt='you are a python expert helping to analyze code')
-    agent = OpenAIAgent(model='gpt-4o', system_prompt='you are a python expert helping to analyze code')
+    agent = OpenAIAgent(model='o3-mini', system_prompt='you are a python expert helping to analyze code')
     res = agent.message(f'''\
 I have reference code for solving a task to collect data from GDC.
 Additionally I have a large collection of attempts/trials to solve the same problem, where each trial may or may not correctly solve the problem.
